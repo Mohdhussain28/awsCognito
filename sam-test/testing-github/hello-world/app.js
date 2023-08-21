ffc2d8523f5efb5e99154430d76d0199dc450ada
Mohdhussain28 <mohdhussain28@outlook.com>
2023-08-21 22:15:35 +0530

    .

---
+++ sam-test/testing-github/hello-world/app.js
@@ -0,0 +1,17 @@
+exports.lambdaHandler = async (event, context) => {
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
